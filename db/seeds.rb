ad21d7a824cf112d0ab4701d34bea8b9cb9243c1
Anthony Malerba <toshy_malerba@hotmail.com>
2021-02-22 17:20:57 +0100

    add seeds database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+City.create(name: 'Le Locle', npa: '2400', id: 1)  
+City.create(name: 'Neuchâtel', npa: '2300', id: 2)  
+City.create(name: 'La Neuveville', npa: '2520', id: 3)  
+City.create(name: 'Le Landeron', npa: '2525', id: 4)  
+City.create(name: 'Fribourg', npa: '1700', id: 5)  
+
+Restaurant.create(name: 'Hotel de la couronne', description: 'Brasserie', city_id: 5)
+Restaurant.create(name: 'Pizzeria Napoli', description: 'Pizzeria napolitaine', city_id: 5)
+Restaurant.create(name: 'Pizzeria des pontains', description: 'Pizzeria', city_id: 5)
+Restaurant.create(name: 'La dolce vita', description: 'Spécialités italiennes', city_id: 5)
+Restaurant.create(name: 'Hotel de l''ours', description: 'Spécialités régionales', city_id: 3)
+Restaurant.create(name: 'Hymmmalaya', description: 'Spécialités népalaises', city_id: 3)
+Restaurant.create(name: 'Chez Maurice', description: 'Traiteur régional', city_id: 3)
+Restaurant.create(name: 'Brasserie de la fontaine', description: 'Brasserie', city_id: 3)
+Restaurant.create(name: 'Sakura', description: 'Chinois', city_id: 2)
+Restaurant.create(name: 'Pékin express', description: 'Chinois', city_id: 2)
